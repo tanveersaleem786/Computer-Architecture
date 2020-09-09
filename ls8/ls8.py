5f2fc28e01f27d0cea91b3cd840390c7c4f7a7a3
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-09-08 21:07:15 -0400

    Day 2: Task Completed

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-
 """Main."""
-
 import sys
-from cpu import *
+# For Day 1
+# from cpu import *
+# cpu.load()
+# cpu.run()
 
-cpu = CPU()
-
-cpu.load()
+# For Day 2
+from cpu2 import *
+# Check the file is enter from command line
+if len(sys.argv) != 2:
+    print("Enter file name.")
+    exit(1)
+cpu = CPU2()
+# Get file name from command line arguments
+cpu.load(sys.argv[1])
 cpu.run()
